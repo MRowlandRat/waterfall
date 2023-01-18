f82f5d7d477b12d882157f9b13f523fab48db54c
Drevian1 <drtaylor@student.neumont.edu>
2023-01-18 09:50:00 -0700

    Slight problem with labels. Button isn't fully tested but the code shows its responding.

---
+++ KeyBoardForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace waterfallPro
+{
+    public partial class KeyBoardForm : Form
+    {
+        public KeyBoardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyBoardForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontDialog2_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Button Game!";
+        }
+
+        private void label2_Click(object sender, KeyPressEventArgs e)
+        {
+           
+
+            if (e.KeyChar == (char)Keys.G)
+            {
+                MessageBox.Show("Enter key pressed");
+            }
+            if (e.KeyChar == 13)
+            {
+                MessageBox.Show("Enter key pressed");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Press and hold the G key to play!";
+        }
+    }
+}
+
+
+        
+
+
+        
+        
+        
+    
+
