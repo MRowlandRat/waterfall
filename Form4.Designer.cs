9ebb9f2656d42694564ab616e1d8d2f3d7350d0b
Drevian1 <drtaylor@student.neumont.edu>
2023-01-18 22:34:17 -0700

    update

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace waterfallPro
 {
-    partial class KeyBoardForm
+    partial class Form4
     {
         /// <summary>
         /// Required designer variable.
@@ -32,54 +32,37 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(372, 24);
+            this.label1.Location = new System.Drawing.Point(371, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.VisibleChanged += new System.EventHandler(this.label1_VisibleChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(378, 124);
+            this.label2.Location = new System.Drawing.Point(379, 123);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.VisibleChanged += new System.EventHandler(this.label2_VisibleChanged);
             // 
-            // label3
-            // 
-            this.label3.Location = new System.Drawing.Point(0, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 23);
-            this.label3.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(337, 230);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 31);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // KeyBoardForm
+            // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "KeyBoardForm";
+            this.Name = "Form4";
             this.Text = "KeyBoardForm";
             this.Load += new System.EventHandler(this.KeyBoardForm_Load);
             this.ResumeLayout(false);
@@ -92,6 +75,5 @@
         private Label label1;
         private Label label2;
         private Label label3;
-        private TextBox textBox1;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace waterfallPro
 {
-    public partial class KeyBoardForm : Form
+    public partial class Form4 : Form
     {
-        public KeyBoardForm()
+        public Form4()
         {
             InitializeComponent();
         }
@@ -28,12 +28,12 @@ namespace waterfallPro
         }
 
 
-        private void label1_Click(object sender, EventArgs e)
+        private void label1_VisibleChanged(object sender, EventArgs e)
         {
             label1.Text = "Button Game!";
         }
 
-        private void label2_Click(object sender, KeyPressEventArgs e)
+        private void label2_VisibledChanged(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == (char)Keys.G)
             {
@@ -45,58 +45,32 @@ namespace waterfallPro
             }
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void label2_VisibleChanged(object sender, EventArgs e)
         {
             label2.Text = "Press and hold the G key to play!";
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+        
 
-        }
-
-        private bool nonNumberEntered = false;
-
-        // Handle the KeyDown event to determine the type of character entered into the control.
-        private void textBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
-        {
-            // Initialize the flag to false.
-            nonNumberEntered = false;
-
-            // Determine whether the keystroke is a number from the top of the keyboard.
-            if (e.KeyCode < Keys.D0 || e.KeyCode > Keys.D9)
-            {
-                // Determine whether the keystroke is a number from the keypad.
-                if (e.KeyCode < Keys.NumPad0 || e.KeyCode > Keys.NumPad9)
-                {
-                    // Determine whether the keystroke is a backspace.
-                    if (e.KeyCode != Keys.Back)
-                    {
-                        // A non-numerical keystroke was pressed.
-                        // Set the flag to true and evaluate in KeyPress event.
-                        nonNumberEntered = true;
-                    }
-                }
-            }
-            //If shift key was pressed, it's not a number.
-            if (Control.ModifierKeys == Keys.Shift)
-            {
-                nonNumberEntered = true;
-            }
-        }
-
-
-        private void textBox1_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        private void label3_KeyPress(object sender, KeyEventArgs e)
         {
             // Check for the flag being set in the KeyDown event.
-            if (nonNumberEntered == true)
+            if (e.KeyCode == Keys.G)
             {
-                // Stop the character from being entered into the control since it is non-numerical.
-                e.Handled = true;
+                int counter = 0;
+
+                while (counter <= 0)
+                {
+                    label3.Text = counter;
+                    counter++;
+                }
+
+                Console.WriteLine();
             }
         }
+        }
     }
-}
+
 
 
         
