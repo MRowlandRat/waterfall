56374d90ab3c9dcc8348fb994a79eb1e9c1536d2
honeybadger448 <36938347+honeybadger448@users.noreply.github.com>
2023-01-21 15:30:30 -0700

    yes

---
+++ Form2.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace waterfallPro
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.word_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Uppercase",
+            "Lowercase",
+            "Reverse",
+            "Duplicate"});
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(800, 450);
+            this.checkedListBox1.TabIndex = 3;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // word_box
+            // 
+            this.word_box.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.word_box.Location = new System.Drawing.Point(0, 419);
+            this.word_box.Name = "word_box";
+            this.word_box.Size = new System.Drawing.Size(800, 31);
+            this.word_box.TabIndex = 4;
+            this.word_box.Text = "Enter Text Here";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.word_box);
+            this.Controls.Add(this.checkedListBox1);
+            this.IsMdiContainer = true;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CheckedListBox checkedListBox1;
+        private TextBox word_box;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace waterfallPro
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string scramble = word_box.Text;
+
+            string input = checkedListBox1.Text;
+
+            switch (input)
+            {
+                
+                case "Uppercase":
+                    word_box.Text = scramble.ToUpper();
+                    break;
+
+                case "Lowercase":
+                    word_box.Text = scramble.ToLower();
+                    break;
+
+                case "Reverse":
+                    char[] scrambleArray = scramble.ToCharArray();
+                    Array.Reverse(scrambleArray);
+                    word_box.Text = new string(scrambleArray); 
+                    break;
+
+                case "Duplicate":
+
+                    scramble = scramble + scramble;
+
+                    word_box.Text = scramble;
+                    break;
+            }
+        }
+    }
+}
